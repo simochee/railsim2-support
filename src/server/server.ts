504c47be284cd3456fd58651cfd83bdfd9b7bcbf
Ryoya Tamura <ryoya.tamura@nulab.com>
2026-04-08 12:31:21 +0900

    revert: triggerCharacters から改行トリガーを削除

--- src/server/server.ts
+++ src/server/server.ts
@@ -24,7 +24,6 @@ connection.onInitialize(() => ({
     textDocumentSync: TextDocumentSyncKind.Full,
     completionProvider: {
       resolveProvider: false,
-      triggerCharacters: ["\n"],
     },
   },
 }));
