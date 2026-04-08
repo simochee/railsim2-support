20a34810c968791c224d0d3a01f506bd5b6cf864
Ryoya Tamura <ryoya.tamura@nulab.com>
2026-04-08 12:27:32 +0900

    feat: 改行時に自動で補完候補を表示

--- src/server/server.ts
+++ src/server/server.ts
@@ -24,6 +24,7 @@ connection.onInitialize(() => ({
     textDocumentSync: TextDocumentSyncKind.Full,
     completionProvider: {
       resolveProvider: false,
+      triggerCharacters: ["\n"],
     },
   },
 }));
